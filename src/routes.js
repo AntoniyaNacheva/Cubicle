0fdb28a5f432c3689a910177f330bcfd811faef2
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-01-23 15:40:25 +0200

    Add details page

--- src/routes.js
+++ src/routes.js
@@ -20,4 +20,6 @@ router.get('/create', cubeController.getCreateCube);
 
 router.post('/create', cubeController.postCreateCube);
 
+router.get('/details/:cubeId', cubeController.getDetails);
+
 module.exports = router;
