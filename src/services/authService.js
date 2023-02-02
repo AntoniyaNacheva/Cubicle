ca4690816104b6a0b79de77fe0e1265272dce76f
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-02-02 14:01:06 +0200

    Register user

---
+++ src/services/authService.js
@@ -0,0 +1,6 @@
+const User = require('../models/User');
+
+exports.getUserByUsername = (username) => User.findOne({ username });
+
+exports.register = (username, password) => User.create({ username, password });
+
