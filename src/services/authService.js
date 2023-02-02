a4abc9e775b2684fd000a03543e1e6131138020c
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-02-02 14:35:02 +0200

    Check user and pass on login

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,3 +4,16 @@ exports.getUserByUsername = (username) => User.findOne({ username });
 
 exports.register = (username, password) => User.create({ username, password });
 
+exports.login = async (username, password) => {
+	const user = await this.getUserByUsername(username);
+
+	const isValid = await user.validatePassword(password);
+	if(!user || !isValid) {
+		throw 'Invalid username or password!';
+	}
+	// if(!(user && user.validatePassword(password))) {
+	// 	throw 'Invalid username or password!';
+	// }    !a || !b === !(a && b)
+
+	return user;
+}
