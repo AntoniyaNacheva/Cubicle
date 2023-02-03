bbe8b4f50b4ae0a9e4c08507668d49c09f0ca921
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-02-03 09:56:55 +0200

    Generate token on login

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,13 @@
 const config = {
 	poduction: {
 		PORT: 1234,
-		DB_URI: 'mongodb://127.0.0.1:27017/cubicle'
+		DB_URI: 'mongodb://127.0.0.1:27017/cubicle',
+		SECRET: 'SOMEPRODSECRET',
 	},
 	development: {
 		PORT: 3000,
-		DB_URI: 'mongodb://127.0.0.1:27017/cubicle'
+		DB_URI: 'mongodb://127.0.0.1:27017/cubicle',
+		SECRET: 'SOMEPRODSECRET',
 	}
 };
 
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,6 @@
 const User = require('../models/User');
+const config = require('../config');
+const jwt = require('../lib/jsonwebtoken');
 
 exports.getUserByUsername = (username) => User.findOne({ username });
 
@@ -8,12 +10,15 @@ exports.login = async (username, password) => {
 	const user = await this.getUserByUsername(username);
 
 	const isValid = await user.validatePassword(password);
-	if(!user || !isValid) {
+	if (!user || !isValid) {
 		throw 'Invalid username or password!';
 	}
 	// if(!(user && user.validatePassword(password))) {
 	// 	throw 'Invalid username or password!';
 	// }    !a || !b === !(a && b)
 
-	return user;
+	const payload = { username: user.username };
+	const token = await jwt.sign(payload, config.SECRET, { expiresIn: '2h' });
+
+	return token;
 }
