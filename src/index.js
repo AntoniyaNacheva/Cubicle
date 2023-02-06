d937c12361b5e5a9a98ec94ef0e47f194ffb61df
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-02-06 20:37:49 +0200

    Add auth middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const routes = require('./routes');
 
+const routes = require('./routes');
 const config = require('./config');
+const authMiddleware = require('./middlewares/authMiddleware');
 const setupViewEngine = require('./config/viewEngine');
 const initDatabase = require('./config/databaseInit');
 
@@ -14,6 +15,7 @@ setupViewEngine(app);
 app.use(express.static('src/public'));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
+app.use(authMiddleware.authentication);
 app.use(routes);
 
 initDatabase()
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
 const accessoryController = require('./controllers/accessoryController');
 const authController = require('./controllers/authController');
+const { isAuthenticated } = require('./middlewares/authMiddleware');
 
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
@@ -19,8 +20,8 @@ router.use('/', authController);
 // 	res.render('create');
 // });
 
-router.get('/cubes/create', cubeController.getCreateCube);
-router.post('/cubes/create', cubeController.postCreateCube);
+router.get('/cubes/create', isAuthenticated, cubeController.getCreateCube);
+router.post('/cubes/create', isAuthenticated, cubeController.postCreateCube);
 router.get('/cubes/:cubeId/details', cubeController.getDetails);
 router.get('/cubes/:cubeId/attach', cubeController.getAttachAccessory);
 router.post('/cubes/:cubeId/attach', cubeController.postAttachAccessory);
